dd68ed2a188c29c07d6ae1c84fc99b805dc26156
Ignacio Cunial <igsecu@hotmail.com>
2022-11-18 16:57:33 -0300

    categories routes - controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const { Router } = require("express");
 const router = Router();
 
 const userRouter = require("./users");
+const categoryRouter = require("./categories");
 
 router.use("/users", userRouter);
+router.use("/categories", categoryRouter);
 
 module.exports = router;

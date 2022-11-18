a58a86895c71fa49c700c35208107e0322303f0d
Ignacio Cunial <igsecu@hotmail.com>
2022-11-18 16:09:42 -0300

    users register login routes - jwt authentication

---
+++ src/controllers/users.js
@@ -0,0 +1,39 @@
+const { User } = require("../db");
+
+const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcryptjs");
+
+const getUserById = async (id) => {
+  try {
+    const dbResult = await User.findByPk(id);
+
+    if (!dbResult) return null;
+
+    const result = {
+      id: dbResult.id,
+      email: dbResult.email,
+    };
+
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const createUser = async (email, password) => {
+  try {
+    const userCreated = await User.create({
+      email,
+      password: await bcrypt.hash(password, 10),
+      id: uuidv4(),
+    });
+    return userCreated;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = {
+  createUser,
+  getUserById,
+};
---
+++ src/routes/users.js
@@ -0,0 +1,71 @@
+const { Router } = require("express");
+const router = Router();
+
+const { v4: uuidv4 } = require("uuid");
+
+const { User } = require("../db");
+const userController = require("../controllers/users");
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email) return res.status(404).json("Falta email del usuario!");
+    if (!password) return res.status(404).json("Falta password del usuario!");
+
+    const userEmail = await User.findOne({ where: { email } });
+    if (!userEmail) throw new Error("Email no encontrado!");
+
+    const passwordMatch = await bcrypt.compare(password, userEmail.password);
+    if (!passwordMatch) throw new Error("Password incorrecto!");
+
+    const userById = await userController.getUserById(userEmail.id);
+
+    const token = jwt.sign(
+      {
+        userId: userById.id,
+        email: userById.email,
+      },
+      "RANDOM_TOKEN",
+      { expiresIn: "24h" }
+    );
+
+    res.status(200).json(token);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) return res.status(404).json("Falta email del usuario!");
+  if (!password) return res.status(404).json("Falta password del usuario!");
+
+  try {
+    const emailExist = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (emailExist) {
+      return res
+        .status(400)
+        .json(
+          "Existe un usuario con esa dirección de email. Prueba con una nueva!"
+        );
+    }
+
+    const userCreated = await userController.createUser(email, password);
+
+    res.status(201).json(userCreated);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+module.exports = router;

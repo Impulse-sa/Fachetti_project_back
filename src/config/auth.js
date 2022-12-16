d171b0e89633554c509421b68b5d984ce157e31b
LHC <lautarohconti@gmail.com>
2022-12-16 00:00:31 -0300

    modified authorization

--- src/config/auth.js
+++ src/config/auth.js
@@ -4,7 +4,8 @@ const { RANDOM_TOKEN } = process.env;
 module.exports = async (req, res, next) => {
   try {
     console.log('req.headers: ', req.headers)
-    const token = await req.headers.authorization.split(" ")[1];
+    // const token = await req.headers.authorization.split(" ")[1];
+    const token = await req.headers.authorization;
 
     const decodedToken = await jwt.verify(token, RANDOM_TOKEN);
 

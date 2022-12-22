d45eecc74ba6a68421aeba5c2f9c62eedbdd8306
LHC <lautarohconti@gmail.com>
2022-12-22 18:53:47 -0300

    convert date in en-GB

--- src/utils/convertDate.js
+++ src/utils/convertDate.js
@@ -1,6 +1,6 @@
 const convertDate = (createdAt)=>{
-    const date = new Date(createdAt).toLocaleDateString('en')
-    const hours = new Date(createdAt).toLocaleTimeString('en')
+    const date = new Date(createdAt).toLocaleDateString('en-GB')
+    const hours = new Date(createdAt).toLocaleTimeString('en-GB')
     return `${date} ${hours}`
 }
 

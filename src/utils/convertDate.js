69ec0076bdeb3879406f0b982e48dcc06909b80f
LHC <lautarohconti@gmail.com>
2022-12-23 15:55:40 -0300

    convertDate in format dd/mm/yy hh:mm

--- src/utils/convertDate.js
+++ src/utils/convertDate.js
@@ -1,7 +1,14 @@
 const convertDate = (createdAt)=>{
-    const date = new Date(createdAt).toLocaleDateString('en-GB')
-    const hours = new Date(createdAt).toLocaleTimeString('en-GB')
-    return `${date} ${hours}`
+    //Parse date
+    const date = new Date(createdAt).toLocaleDateString('en-GB').split('/')
+    const ddmm = date.slice(0,2).join('/')
+    const yy = date[2].slice(2,4).toString()
+    const ddmmyy = ddmm + '/' + yy
+
+    //Parse time
+    const hours = new Date(createdAt).toLocaleTimeString('en-GB').split(':').slice(0,2).join(':')
+
+    return `${ddmmyy} ${hours}`
 }
 
 module.exports={ convertDate }
